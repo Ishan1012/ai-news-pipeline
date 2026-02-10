730c44f354887ab2a8e96cc4e26b4b9fbe50af65
Ishan1012 <ishandwivedi1210@gmail.com>
2026-02-11 00:50:00 +0530

    added requirements.txt

--- mcp_server.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from db import collection
-
-app = FastAPI()
-
-@app.get("/news")
-def get_news(limit: int = 10):
-    return list(collection.find({}, {"_id": 0}).limit(limit))
